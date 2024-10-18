5c2b65e20392b294bf850ab81b3e351f3c17f270
DanilSereda <seredadanil944@gmail.com>
2024-10-18 18:16:35 +0300

    18.10.2024 ПЗ11

--- WebBooks/WebBooks/settings.py
+++ WebBooks/WebBooks/settings.py
@@ -1,9 +1,6 @@
 from pathlib import Path
 import os
-import dj_database_url
-from dotenv import load_dotenv  
 
-load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -13,21 +10,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 #SECRET_KEY = 'django-insecure-9l!9y%9@+0cybnd%+wg6-!l(7_3y@a=^vf$6it6vyk6wa#v5uc'
-SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY',
-'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
-SECRET_KEY = os.environ.get("SECRET_KEY")  
+SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY','cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
-if os.environ.get("DEBUG") == "False":
-    DEBUG = False
-else:
-    DEBUG = True
-
-# SECURITY WARNING: don't run with debug turned on in production!
 #DEBUG = True
 DEBUG = bool(os.environ.get('DJANGO_DEBUG', True))
-SESSION_COOKIE_SECURE = True  
-CSRF_COOKIE_SECURE = True
-ALLOWED_HOSTS = ["127.0.0.1", "DabilSereda.pythonanywhere.com"]
+
+
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -79,19 +68,9 @@ WSGI_APPLICATION = 'WebBooks.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    "default": {  
-        "ENGINE": "django.db.backends.mysql",  
-        "NAME": os.getenv("MYSQL_DBNAME"),  
-        "USER": os.getenv("MYSQL_USER"),  
-        "PASSWORD": os.getenv("MYSQL_PASSWORD"),  
-        "HOST": os.getenv("MYSQL_HOST"),  
-        "OPTIONS": {  
-            "init_command": "SET NAMES 'utf8mb4';SET sql_mode = 'STRICT_TRANS_TABLES'",  
-            "charset": "utf8mb4",  
-        },  
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
@@ -131,26 +110,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / "static/"  
-MEDIA_URL = "media/"  
-MEDIA_ROOT = BASE_DIR / "media/"
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static"),
-]
-
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+LOGIN_REDIRECT_URL = '/'
 
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-# Статичные файлы (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.10/howto/static-files/
-# Абсолютный путь к каталогу, в котором collectstatic
-# будет собирать статические файлы для развертывания.
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-# Упрощённая обработка статическая файлов.
-# https://warehouse.python.org/project/whitenoise/
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
--- WebBooks/WebBooks/urls.py
+++ WebBooks/WebBooks/urls.py
@@ -1,20 +1,31 @@
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path
 from catalog import views
+from django.urls import  re_path
+from django.urls import path, include
 
 urlpatterns = [
- path('', views.index, name='index'),
- path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('admin/', admin.site.urls),
+    re_path(r'^books/$', views.BookListView.as_view(), name='books'),
+    re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
+    re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
+    path('authors_add/', views.authors_add, name='authors_add'),
+    path('edit1/<int:id>/', views.edit1, name='edit1'), 
+    path('create/', views.create, name='create'), 
+    path('delete/<int:id>/', views.delete, name='delete'),
+]
+
+urlpatterns += [
  path('accounts/', include('django.contrib.auth.urls')),
- re_path(r'^books/$', views.BookListView.as_view(), name='books'),
- re_path(r'^book/(?P<pk>\d+)$', views.BookDetailView.as_view(), name='book-detail'),
- re_path(r'^authors/$', views.AuthorListView.as_view(), name='authors'),
- path('mybooks/', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
- path('authors_add/', views.authors_add, name='authors_add'),
- path('edit1/<int:id>/', views.edit1, name='edit1'),
- path('create/', views.create, name='create'),
- path('delete/<int:id>/', views.delete, name='delete'),
- re_path(r'^book/create/$', views.BookCreate.as_view(), name='book_create'),
- re_path(r'^book/update/(?P<pk>\d+)$', views.BookUpdate.as_view(), name='book_update'),
- re_path(r'^book/delete/(?P<pk>\d+)$', views.BookDelete.as_view(), name='book_delete'),
+]
+
+urlpatterns += [
+    re_path(r'^mybooks/$', views.LoanedBooksByUserListView.as_view(), name='my-borrowed'),
+]
+
+urlpatterns += [ 
+    re_path(r'^book/create/$', views.BookCreate.as_view(), name='book_create'),
+    re_path(r'^book/update/(?P<pk>\d+)$', views.BookUpdate.as_view(), name='book_update'), 
+    re_path(r'^book/delete/(?P<pk>\d+)$', views.BookDelete.as_view(), name='book_delete'), 
 ]
