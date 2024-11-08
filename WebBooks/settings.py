b1bb24cb1a51469269e8833bb20f38742e5bd8aa
DanilSereda <seredadanil944@gmail.com>
2024-11-08 13:23:20 +0300

    08.11.2024 Sereda Danil ЛЗ17 научился пользоваться моделями данных.

--- WebBooks/settings.py
+++ WebBooks/settings.py
@@ -26,7 +26,7 @@ INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
-    'bootstrap5',
+    # 'bootstrap5',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
