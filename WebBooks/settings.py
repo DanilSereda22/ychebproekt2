1558c27dec073ba26225b28cb205fea1c351d8f6
SeredaDanil <seredadanil944@gmail.com>
2024-12-10 10:02:27 +0300

    11.11.2024 Sereda Danil ЛЗ18  научился правильно разрабатывать структуру сайта

--- WebBooks/settings.py
+++ WebBooks/settings.py
@@ -67,6 +67,7 @@ WSGI_APPLICATION = 'WebBooks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
